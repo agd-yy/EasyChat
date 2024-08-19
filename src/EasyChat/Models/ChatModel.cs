e7d2fd4f6562206ac88140d08fce20c1fa0a3d04
zggsong <zggsong@foxmail.com>
2024-08-19 18:01:52 +0800

    feat: new ui

--- src/EasyChat/Models/ChatModel.cs
+++ src/EasyChat/Models/ChatModel.cs
@@ -1,64 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace EasyChat.Models
+namespace EasyChat.Models;
+
+public partial class ChatModel : ObservableObject
 {
-    public class ChatModel : ObservableObject
-    {
-        // 昵称
-        private string _nickName = "";
-        public string NickName
-        {
-            get => _nickName;
-            set => SetProperty(ref _nickName, value);
-        }
-        private string _uid = "";
-        public string Uid
-        {
-            get => _uid;
-            set => SetProperty(ref _uid, value);
-        }
+    [ObservableProperty] private string _color = "#ffad2c";
 
-        private string _message = "";
-        public string Message
-        {
-            get => _message;
-            set => SetProperty(ref _message, value);
-        }
+    // 一般来说，这个名字就是显示在界面上的聊天对象的名字，或者群名字
+    [ObservableProperty] private string _groupName = "";
 
-        private string _color = "#ffad2c";
-        public string Color
-        {
-            get => _color;
-            set => SetProperty(ref _color, value);
-        }
 
-        private string _tagName = "";
-        public string TagName
-        {
-            get => _tagName;
-            set => SetProperty(ref _tagName, value);
-        }
+    [ObservableProperty] private string _image = "";
 
-        private int _messageCount;
-        public int MessageCount
-        {
-            get => _messageCount;
-            set => SetProperty(ref _messageCount, value);
-        }
+    [ObservableProperty] private string _message = "";
 
-        private string _image = "";
-        public string Image
-        {
-            get => _image;
-            set => SetProperty(ref _image, value);
-        }
+    [ObservableProperty] private int _messageCount;
 
-        // 一般来说，这个名字就是显示在界面上的聊天对象的名字，或者群名字
-        private string _groupName = "";
-        public string GroupName
-        {
-            get => _groupName;
-            set => SetProperty(ref _groupName, value);
-        }
-    }
+    // 昵称
+    [ObservableProperty] private string _nickName = "";
+
+    [ObservableProperty] private string _tagName = "";
+    [ObservableProperty] private string _uid = "";
 }
